4ddfb88e8a610464556c1efd87d10d3426cefc07
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-12-18 16:09:25 +0800

    fixed home page

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
-from .models import About
 
 class HomeView(TemplateView):
-    template_name = "templates/home.html"
+    template_name = "home.html"
 
 class AboutView(TemplateView):
-    template_name = "templates/about.html"
+    template_name = "about.html"
+  
