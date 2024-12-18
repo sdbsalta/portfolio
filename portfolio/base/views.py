82ddaa2f7d667ece70c0e360d1d0ee011f0e4d23
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-12-18 15:59:11 +0800

    added views

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+from .models import About
+
+class HomeView(TemplateView):
+    template_name = "templates/home.html"
+
+class AboutView(TemplateView):
+    template_name = "templates/about.html"
